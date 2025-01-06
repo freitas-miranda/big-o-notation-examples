14e9b0a9d32b06c08d0de6f882cc94e6b3a8ea02
Alan Miranda <freitas.miranda@gmail.com>
2025-01-05 23:59:00 -0400

    docs: adiciona exponencial O(2^n)

--- README.md
+++ README.md
@@ -8,5 +8,3 @@ O(n) - linear
 O(n log n) - log linear
 O(n²) - quadrática
 O(2^n) - exponencial
-
-https://media.licdn.com/dms/document/media/v2/D4D1FAQEN8XwiNLSBcg/feedshare-document-pdf-analyzed/B4DZQKjIzwGkAY-/0/1735343815515?e=1736985600&v=beta&t=NZiUQuqFUfM_zZrXUgHqeeOR88IVtMSmeeSpy-ovwrM
---
+++ exponencial.js
@@ -0,0 +1,26 @@
+/*
+# Exponencial O(2^n)
+É um dos piores casos de complexidade de algoritmos.
+Ocorre quando o tempo de execução de um algoritmo dobra a cada aumento unitário no tamanho da entrada n.
+Um exemplo clássico é o cálculo da sequência de Fibonacci.
+A abordagem recursiva para calcular o n-ésimo número da sequência de Fibonacci é um exemplo clássico de complexidade O(2^n).
+*/
+
+let execucoes = 0;
+
+function fibonacci(numero) {
+  execucoes++;
+  console.log(`${execucoes}ª execução!`);
+
+  if (numero < 1) {
+    return 0;
+  } else if (numero <= 2) {
+    return 1;
+  } 
+
+  return fibonacci(numero - 1) + fibonacci(numero - 2);
+}
+
+const posicao = 6;
+const resultado = fibonacci(posicao);
+console.log(`O ${posicao}º número da sequência de Fibonacci é ${resultado}`);
